54c29bbc9e51e11dba32e54948ff4c857949a7d4
Alberto Rossotto <alberto.rossotto@gmail.com>
2024-01-26 08:29:35 +0100

    first commit

---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/bin/zsh
+
+# start ollama in background with stderr redirected to stdin
+ollama serve 2>&1 &
+
+# ollama's pid (to kill the process later)
+OLLAMA_PID=$!
+
+# start the service
+python -m streamlit run app.py
+
+# shut down ollama
+kill -9 $OLLAMA_PID
